f9db72835f6e33c46652002bd01b62a5c3e21f43
gabymcanales <gabycanales728@gmail.com>
2024-12-02 15:07:54 -0600

    App web para negocio

---
+++ App_Web_Negocio/js/administrator.js
@@ -0,0 +1,5 @@
+//CONEXION ENTRE PANTALLAS
+const btn_editar = document.getElementById('btn_editar');
+btn_editar.addEventListener('click', function () {
+    window.location.href = 'editarRegistro.html';
+});
